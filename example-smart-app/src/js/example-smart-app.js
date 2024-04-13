e04da3d53b727eb4a65114dd3650b66782dcb2ad
praveen-sc <164640489+praveen-sc@users.noreply.github.com>
2024-04-13 12:14:48 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -34,7 +34,7 @@
 
           var fname = '';
           var lname = '';
-          var status = patient.status;
+          var status = medication.status;
 
           if (typeof patient.name[0] !== 'undefined') {
             fname = patient.name[0].given.join(' ');
